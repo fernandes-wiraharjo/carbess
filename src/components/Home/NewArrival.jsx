39677d347511a58e677c93f9d73ab876a5c65d21
fernandes-wiraharjo <fernandeswiraharjo@gmail.com>
2023-09-17 01:56:56 +0700

    patching api new arrival and recommended items

--- src/components/Home/NewArrival.jsx
+++ src/components/Home/NewArrival.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -9,114 +10,65 @@ import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import './NewArrival.css';
 
 export default function NewArrival() {
-    return (
+    const [car, setCar] = useState([]);
+
+    useEffect(() => {
+        cars()
+    }, []);
+
+    const cars = async () => {
+        const response = await fetch('http://localhost:3000/cars/new-arrival');
+        setCar(await response.json());
+    };
+
+    const formattedNumber = (value) => {
+        return Intl.NumberFormat("id-ID", { maximumFractionDigits: 2 }).format(value);
+    };
+
+    return (        
         <section className='NewArrival'>
             <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
                 New Arrival
             </Typography>
             <Grid container spacing={2} mt={2}>
-                <Grid item xs={12} sm={6} lg={3}>
-                    <Card>
-                        <NavLink to="/car-detail">
-                            <CardActionArea>
-                                <CardMedia
-                                    component="img"
-                                    height="250"
-                                    image="/images/new_arrivals/1.jpg"
-                                    alt="new arrival item 1"
-                                />
-                                <CardContent>
-                                    <Typography gutterBottom variant="h7" sx={{ fontWeight: 'bold' }} component="div">
-                                        Xpander Ultimate 2018
-                                    </Typography>
-                                    <Typography variant="h6" color="text.secondary" sx={{ fontWeight: 'bold' }}>
-                                        Rp. 225.000.000
-                                    </Typography>
-                                </CardContent>
-                            </CardActionArea>
-                        </NavLink>
-                        <CardActions>
-                            <Button aria-label="whatsapp" variant="outlined" sx={{ width: '100%' }} color="success" size="medium">
-                                <WhatsAppIcon />
-                            </Button>
-                        </CardActions>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} sm={6} lg={3}>
-                    <Card>
-                        <CardActionArea>
-                            <CardMedia
-                                component="img"
-                                height="250"
-                                image="/images/new_arrivals/2.jpg"
-                                alt="new arrival item 2"
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h7" sx={{ fontWeight: 'bold' }} component="div">
-                                    Innova G 2018
-                                </Typography>
-                                <Typography variant="h6" color="text.secondary" sx={{ fontWeight: 'bold' }}>
-                                    Rp. 290.000.000
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                        <CardActions>
-                            <Button aria-label="whatsapp" variant="outlined" sx={{ width: '100%' }} color="success" size="medium">
-                                <WhatsAppIcon />
-                            </Button>
-                        </CardActions>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} sm={6} lg={3}>
-                    <Card>
-                        <CardActionArea>
-                            <CardMedia
-                                component="img"
-                                height="250"
-                                image="/images/new_arrivals/3.jpg"
-                                alt="new arrival item 3"
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h7" sx={{ fontWeight: 'bold' }} component="div">
-                                    Camry 2011 V
-                                </Typography>
-                                <Typography variant="h6" color="text.secondary" sx={{ fontWeight: 'bold' }}>
-                                    Rp. 165.000.000
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                        <CardActions>
-                            <Button aria-label="whatsapp" variant="outlined" sx={{ width: '100%' }} color="success" size="medium">
-                                <WhatsAppIcon />
-                            </Button>
-                        </CardActions>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} sm={6} lg={3}>
-                    <Card>
-                        <CardActionArea>
-                            <CardMedia
-                                component="img"
-                                height="250"
-                                image="/images/new_arrivals/4.jpg"
-                                alt="new arrival item 4"
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h7" sx={{ fontWeight: 'bold' }} component="div">
-                                    HRV E CVT 2015
-                                </Typography>
-                                <Typography variant="h6" color="text.secondary" sx={{ fontWeight: 'bold' }}>
-                                    Rp. 235.000.000
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                        <CardActions>
-                            <Button aria-label="whatsapp" variant="outlined" sx={{ width: '100%' }} color="success" size="medium">
-                                <WhatsAppIcon />
-                            </Button>
-                        </CardActions>
-                    </Card>
-                </Grid>
+                {car.length > 0 ? 
+                    car.map((data) => {
+                        return (
+                            <Grid item xs={12} sm={6} lg={3} key={data._id}>
+                                <Card>
+                                    <NavLink to="/car-detail">
+                                        <CardActionArea>
+                                            <CardMedia
+                                                component="img"
+                                                height="250"
+                                                image={ data.images[0] ? data.images[0].image : '' }
+                                                alt={`image ${data.name}`}
+                                            />
+                                            <CardContent>
+                                                <Typography gutterBottom variant="h7" sx={{ fontWeight: 'bold' }} component="div">
+                                                    { data.name }
+                                                </Typography>
+                                                <Typography variant="h6" color="text.secondary" sx={{ fontWeight: 'bold' }}>
+                                                    Rp. { formattedNumber(data.price) }
+                                                </Typography>
+                                            </CardContent>
+                                        </CardActionArea>
+                                    </NavLink>
+                                    <CardActions>
+                                        <Button aria-label="whatsapp" variant="outlined" sx={{ width: '100%' }} color="success" size="medium">
+                                            <WhatsAppIcon />
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                        )
+                    }) :
+                    <Grid item xs={12} sx={{ textAlign: 'center' }}>
+                        <Typography variant="h7" color="text.secondary" sx={{ fontWeight: 'bold' }}>
+                            NO DATA AVAILABLE
+                        </Typography>
+                    </Grid>
+                }
             </Grid>
         </section>
     );
--- src/components/Home/RecommendedItem.jsx
+++ src/components/Home/RecommendedItem.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -8,6 +9,21 @@ import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import './RecommendedItem.css';
 
 export default function RecommendedItem() {
+    const [car, setCar] = useState([]);
+
+    useEffect(() => {
+        cars()
+    }, []);
+
+    const cars = async () => {
+        const response = await fetch('http://localhost:3000/cars/recommended-item');
+        setCar(await response.json());
+    };
+
+    const formattedNumber = (value) => {
+        return Intl.NumberFormat("id-ID", { maximumFractionDigits: 2 }).format(value);
+    };
+
     return (
         <section className='RecommendedItem'>
             <div id="header">
@@ -25,7 +41,43 @@ export default function RecommendedItem() {
                 >{`Tampilkan Semua Mobil >>`}</Button>
             </div>
             <Grid container spacing={2} mt={2}>
-                <Grid item xs={12} sm={6} lg={2.4}>
+                {car.length > 0 ? 
+                    car.map((data) => {
+                        return (
+                            <Grid item xs={12} sm={6} lg={2.4} key={data._id}>
+                                <Card>
+                                    <CardActionArea>
+                                        <CardMedia
+                                            component="img"
+                                            height="200"
+                                            image={ data.images[0] ? data.images[0].image : '' }
+                                            alt={`image ${data.name}`}
+                                        />
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h7" sx={{ fontWeight: 'bold' }} component="div">
+                                                { data.name }
+                                            </Typography>
+                                            <Typography variant="h6" color="text.secondary" sx={{ fontWeight: 'bold' }}>
+                                                Rp. { formattedNumber(data.price) }
+                                            </Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                    <CardActions>
+                                        <Button aria-label="whatsapp" variant="outlined" sx={{ width: '100%' }} color="success" size="medium">
+                                            <WhatsAppIcon />
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                        )
+                    }) :
+                    <Grid item xs={12} sx={{ textAlign: 'center' }}>
+                        <Typography variant="h7" color="text.secondary" sx={{ fontWeight: 'bold' }}>
+                            NO DATA AVAILABLE
+                        </Typography>
+                    </Grid>
+                }
+                {/* <Grid item xs={12} sm={6} lg={2.4}>
                     <Card>
                         <CardActionArea>
                             <CardMedia
@@ -149,7 +201,7 @@ export default function RecommendedItem() {
                             </Button>
                         </CardActions>
                     </Card>
-                </Grid>
+                </Grid> */}
             </Grid>
             <Button id='btnShowAllBottom' variant="contained" size="small" sx={{ 
                 backgroundColor: '#000',
